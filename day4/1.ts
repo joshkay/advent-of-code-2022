127e260debe325d92b143ebf7f723146ea6c07b1
Josh Kay <josh.m.kay@gmail.com>
2022-12-05 10:50:09 -0500

    day 4

---
+++ day4/1.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+
+const input: string = fs.readFileSync('day4/input.txt', 'utf8');
+const lines = input.split('\n');
+
+const doSectionsOverlap = (a: string, b: string): boolean => {
+  const aMin = parseInt(a.split('-')[0]);
+  const aMax = parseInt(a.split('-')[1]);
+  const bMin = parseInt(b.split('-')[0]);
+  const bMax = parseInt(b.split('-')[1]);
+
+  if (aMin >= bMin && aMax <= bMax ||
+    bMin >= aMin && bMax <= aMax) {
+      console.log({a, b})
+    return true;
+  }
+  return false;
+}
+
+const numOverlappingSections = lines.reduce((acc, line) => {
+  const sectionRange = line.split(',');
+  return doSectionsOverlap(sectionRange[0], sectionRange[1]) ? 1 + acc : acc;
+}, 0)
+
+console.log({numOverlappingSections})
