3fd4b55c4177158c6179604b06756f5aeababe8d
Josh Kay <josh.m.kay@gmail.com>
2022-12-06 15:38:41 -0500

    day 6

---
+++ day6/1.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+
+const input: string = fs.readFileSync('day6/input.txt', 'utf8');
+
+const NUM_UNIQUE_CHARS = 14;
+
+let numCharsToProcess;
+
+let uniqueChars: string[] = [];
+input.split('').forEach((char, index) => {
+  const uniqueCharIndex = uniqueChars.indexOf(char);
+  if (uniqueCharIndex !== -1) {
+    uniqueChars.splice(0, uniqueCharIndex + 1);
+  }
+
+  uniqueChars.push(char);
+
+  if (uniqueChars.length === NUM_UNIQUE_CHARS && numCharsToProcess === undefined) {
+    numCharsToProcess = index + 1;
+  }
+});
+
+console.log({numCharsToProcess})
